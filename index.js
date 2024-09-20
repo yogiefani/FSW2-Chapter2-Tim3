a9c32d015bc7bf1d7c9cdc0cd07ed025813f601d
haikalnuril <haikal.nuril23@gmail.com>
2024-09-20 19:43:30 +0700

    feat: error massage

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-let button = document.getElementById('button-hobby')
+let button = document.getElementById('button-hobby');
 
 button.addEventListener('click', ()=> {
-    let hobby = document.getElementById('hobby')
-    let inputHobby = document.getElementById('input-hobby')
+    let hobby = document.getElementById('hobby');
+    let inputHobby = document.getElementById('input-hobby');
+    let errorHobby = document.getElementById('error-hobby');
 
     if (inputHobby.value !== '') {
         hobby.innerHTML = inputHobby.value;
+        errorHobby.innerHTML = '';
+        inputHobby.style.borderColor = "black";
+    }else{
+        inputHobby.style.borderColor = "IndianRed";
+        errorHobby.innerHTML = "Inputan hobby perlu diisi!";
+        errorHobby.style.color = "red";
     }
     
-    inputHobby.value = ''
+    inputHobby.value = '';
 })
